b54c6e7ca5f5a75f786d630c8778501f65ddc529
Sterv99 <xherrg00@vutbr.cz>
2023-05-17 22:19:20 +0200

    recorder - new control prot

--- recorder/src/DDS/recorder/controller.cpp
+++ recorder/src/DDS/recorder/controller.cpp
@@ -1,6 +1,7 @@
 #include <DDS/recorder/controller.hpp>
-#include <DDS/recorder/recorder.hpp>
+#include <DDS/core/flight_data/client.hpp>
 #include <DDS/core/media/manager.hpp>
+#include <DDS/core/client_pool.hpp>
 #include <DDS/websocket/json.hpp>
 #include <chrono>
 #include <ctime>
@@ -9,25 +10,58 @@
 
 using json = nlohmann::json;
 
-bool RecorderController::handle_string(const ClientID_t, std::string const& msg)
+bool RecorderController::handle_string(const ClientID_t cid, std::string const& msg)
 {
+    auto& cpool = client_pool::get();
     try
     {
         json j = json::parse(msg);
 
-        if(j["type"] == "media_record")
+        if(j["type"] == "media_record_get")
         {
-            ClientID_t drone_id = j["data"]["drone_stream_id"];
+            ClientID_t drone_id = cid_from_hex(j["data"]["drone_stream_id"]);
+            j = {
+                { "type", "media_record_get_resp" },
+                { "data", bool(recorders.count(drone_id)) }
+            };
 
-            auto curr_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+            std::dynamic_pointer_cast<FlightDataClient>(cpool.client(cid))->send(j.dump());
+            return true;
+        }
+        else if(j["type"] == "media_record_set")
+        {
+            ClientID_t drone_id = cid_from_hex(j["data"]["drone_stream_id"]);
+            bool state = j["data"]["state"];
 
-            std::ostringstream oss;
-            oss << std::put_time(std::localtime(&curr_time), "%Y.%m.%d-%T");
-            oss << "-";
-            oss << cid_to_hex(drone_id);
-            oss << ".mp4";
+            if(recorders.count(drone_id) == 0 && state)
+            {
+                auto curr_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-            media_manager::get().pipe(drone_id)->add_writer(std::make_shared<media_recorder>(oss.str()));
+                std::ostringstream oss;
+                oss << std::put_time(std::localtime(&curr_time), "%Y%m%d-%H%M%S");
+                oss << "-";
+                oss << cid_to_hex(drone_id);
+                oss << ".mp4";
+
+                recorders[drone_id] = std::make_shared<media_recorder>(oss.str());
+                media_manager::get().pipe(drone_id)->add_writer(recorders[drone_id]);
+            }
+            else if(recorders.count(drone_id) > 0 && !state)
+            {
+                media_manager::get().pipe(drone_id)->del_writer(recorders[drone_id]);
+                recorders.erase(drone_id);
+            }
+            else
+            {
+                //ignored
+            }
+            
+            j = {
+                { "type", "media_record_set_resp" },
+                { "data", nullptr }
+            };
+
+            std::dynamic_pointer_cast<FlightDataClient>(cpool.client(cid))->send(j.dump());
             return true;
         }
     }
--- recorder/src/DDS/recorder/controller.hpp
+++ recorder/src/DDS/recorder/controller.hpp
@@ -2,11 +2,15 @@
 #define RECORDER_CONTROLLER_HPP
 
 #include <DDS/core/flight_data/string_handler.hpp>
+#include <DDS/recorder/recorder.hpp>
+#include <map>
 
 class RecorderController : public StringHandler
 {
 public:
     bool handle_string(const ClientID_t, std::string const&);
+private:
+    std::map<ClientID_t, std::shared_ptr<media_recorder>> recorders;
 };
 
 #endif
